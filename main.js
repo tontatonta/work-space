7efd6dea43fc66829952198d8b51c212f7939d2f
Hinata Tezuka <hinata.tezka.foreign@gmail.com>
2024-03-04 18:33:16 +0900

    a

--- main.js
+++ main.js
@@ -41,9 +41,10 @@ document.addEventListener('DOMContentLoaded', function() {
       return tetoriminos[num]; // ここが改善点です
     }
   
+    // 固定されたテトリミノの配置を記憶
     let field = Array(20).fill().map(() => Array(10).fill(0));
   
-    // positionはfield上
+    // 落ちてくるテトリミノの今の座標を取得
     function getxy(tetorimino){
         const twidth = 4
         const theight = 4
@@ -57,10 +58,13 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         return position
     }
-    function drawTetromino(context, pattern) {
-      context.fillStyle = '#ff0000'; // テトリミノの色
+    
+    // テトリミノを描画
+    function drawTetromino(context, position, color) {
+      context.fillStyle = color; // テトリミノの色
       const cellSize = 20; // テトリミノのセルのサイズ
-      let position = getxy(pattern)
+      // let position = getxy(pattern)
+      // console.log(position)
   
       for (let i = 0; i < position.length; i++) {
           let x = position[i][0]
@@ -69,36 +73,53 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     }
   
+    // 衝突判定
+    // function judge(x, y, field){
+    //   return field[x][y] == 0
+    // }
+  
+    // テトリミノを下に落とす
+    function under(position){
+  
+      // for(let i = 0; i < position.length; i++){
+      //     //下にブロックがあったら
+      //     if (judge(position[i][0], position[i][1]+1)){
+      //         return position
+      //     }
+      // }
+  
+      newPosition = []
+  
+      for (let j = 0; j < position.length; j++){
+          console.log(position[j][1])
+          newPosition.push([position[j][0], position[j][1]+1])
+      }
+  
+      return newPosition
+    }
+  
     // canvasの呼び出し
     const mainCanvas = document.getElementById('tetrisCanvas');
   
     // ↓canvasで2次元描画をするために必要らしい
     const context = mainCanvas.getContext('2d');
   
+    // 使う色
+    const red = '#ff0000'
+    const gray = '#CCCCCC'
+  
     /* 0~4のランダムな整数を取得して、
        ランダムなテトリミノを１つ取得 */
     let randomNumber = Math.floor(Math.random() * 5);
-    const tetrominoPattern = tetorimino(randomNumber);
+    let tetrominoPattern = tetorimino(randomNumber);
   
     // グレーの背景を塗りつぶす
-    context.fillStyle = '#CCCCCC'; // グレー色
+    context.fillStyle = gray; // グレー色
     context.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
   
-    // テトリミノを描画
-    drawTetromino(context, tetrominoPattern);
-  
-    // 下に落ちる
-    function under(field, position){
-      for(var i = 0; i < position.length; i++){
-          //下にブロックがあったら
-          if (judge(position[i][0], position[i][1]+1)){
-              return position
-          }
-      }
-      for (let j = 0; j < position.length; j++){
-          position[j][1] += 1
-      }
-      return position
-    }
+    // 下に落とす動作
+    drawTetromino(context, getxy(tetrominoPattern), red);
+    drawTetromino(context, getxy(tetrominoPattern), gray);
+    drawTetromino(context, under(getxy(tetrominoPattern)), red);
   
   });
