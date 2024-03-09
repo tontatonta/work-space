9215958d2f051c4a295d25c968732f11e9c596a6
Hinata Tezuka <hinata.tezka.foreign@gmail.com>
2024-03-09 17:33:12 +0900

    modify rotate-function

--- main.js
+++ main.js
@@ -209,10 +209,10 @@ document.addEventListener('DOMContentLoaded', function() {
       }
   
       for (let j = 0; j < position.length; j++){
-          let x = position[j][0]-(centerx/4)
-          let y = position[j][1]-(centery/4)
+          let x = Math.floor(position[j][0]-(centerx/4))
+          let y = Math.floor(position[j][1]-(centery/4))
           let X = Math.floor(x*Math.cos(Math.PI/2) - y*Math.sin(Math.PI/2) + (centerx/4))
-          let Y = Math.floor(y*Math.cos(Math.PI/2) + x*Math.sin(Math.PI/2) + (centery/4))
+          let Y = Math.floor(y*Math.cos(Math.PI/2) + x*Math.sin(Math.PI/2) + (centery/4)+0.9)
           newPosition.push([X,Y])
       }
       newPosition = revisePositionIfOverflow(newPosition)
